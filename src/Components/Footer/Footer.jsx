20e85a2268aae0e83ea4a786c31ad0d137e2f33c
Santiago Cortina <santicortina@hotmail.com>
2023-10-23 19:16:35 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     return (
         <>
             <footer>
-                <p>Created by Santiago Cortina | All rights reserved</p>
+{/*                 <p>Created by Santiago Cortina | All rights reserved</p> */}
+                <p>Creado por Santiago Cortina | Todos los derechos reservados</p>
             </footer>
         </>
     )
