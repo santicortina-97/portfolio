4ecd7267dc866a5886d25f02d635f3ed41d629e6
Santiago Cortina <santicortina@hotmail.com>
2024-02-29 09:31:17 -0300

    CV

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
             <div className='containerIntro'>
                 <div>
                     <img src={perfil} alt="" />
-                        <a href="https://drive.google.com/file/d/1OeOTm0dcrRVbydMzRQDAkiKMcPTzbEWZ/view?usp=sharing" target='blank' >
+                        <a href="https://drive.google.com/file/d/1bfnrZ-twGz9oIB5lfUlGqWY1UvSiP3Nt/view?usp=sharing" target='blank' >
                         <button className='curriculum'>
                             <span>CV</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
