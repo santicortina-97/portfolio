6d2b0b4f59bac380540f0a055d571cb5d98655b2
Santiago Cortina <santicortina@hotmail.com>
2025-06-25 13:34:14 -0300

    cv argentina

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
             <div className='containerIntro'>
                 <div>
                     <img src={perfil} alt="" />
-                        <a href="https://drive.google.com/file/d/1MCNjQC85znOGX-xyIjCepIeubJr_CRHp/view?usp=sharing" target='blank' >
+                        <a href="https://drive.google.com/file/d/1FQH5AC4NlZh17kXxJCtUVHMPqEo1ZQwd/view?usp=sharing" target='blank' >
                         <button className='curriculum'>
                             <span>CV</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
